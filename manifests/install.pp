05252092c49bdc77765787ff92a7495cdfcd7b19
ch1aki <sugawara256@gmail.com>
2019-01-17 23:15:13 +0900

    add requirement package
    libpcap is needed by tcpdp.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,9 +2,12 @@ class tcpdp::install(
   $version = $tcpdp::version,
 ) {
 
+  package { 'libpcap': }
+
   package { 'tcpdp':
     provider => rpm,
     source   => "https://github.com/k1LoW/tcpdp/releases/download/v${version}/tcpdp-${version}-1.el7.x86_64.rpm",
+    require  => Package['libpcap'],
   }
 
 }
