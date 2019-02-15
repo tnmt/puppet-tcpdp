52ef9d62d145552f344d9a6ac64053a15a171e5a
ch1aki <sugawara256@gmail.com>
2019-02-15 13:39:28 +0900

    add service_ensure and service_enable param

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,10 +1,13 @@
 class tcpdp::service(
-  $interfaces = $tcpdp::interfaces,
+  $interfaces     = $tcpdp::interfaces,
+  $service_ensure = $tcpdp::service_ensure,
+  $service_enable = $tcpdp::service_enable,
 ) {
 
   $interfaces.each |String $if| {
     service { "tcpdp-${if}":
-      ensure    => 'running',
+      ensure => $service_ensure,
+      enable => $service_enable,
       subscribe => File["/usr/lib/systemd/system/tcpdp-${if}.service"],
     }
   }
