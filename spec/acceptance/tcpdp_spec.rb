2a242e9e844167b692c1eb28d0bce2a78a2001cd
Shinya Tsunematsu <s@tnmt.info>
2019-03-06 22:55:27 +0900

    fix rubocop

--- spec/acceptance/tcpdp_spec.rb
+++ spec/acceptance/tcpdp_spec.rb
@@ -8,18 +8,18 @@ describe 'stns::server class' do
     EOS
   end
 
-  it 'should work without errors' do
+  it 'work without errors' do
     result = apply_manifest(manifest, acceptable_exit_codes: [0, 2], catch_failures: true)
     expect(result.exit_code).not_to eq 4
     expect(result.exit_code).not_to eq 6
   end
 
-  it 'should run a second time without changes' do
+  it 'run a second time without changes' do
     result = apply_manifest(manifest)
     expect(result.exit_code).to eq 0
   end
 
   describe package('tcpdp') do
-    it { should be_installed }
+    it { is_expected.to be_installed }
   end
 end
